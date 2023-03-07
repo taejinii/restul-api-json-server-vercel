6a36a81ff9d532cc0302047ee8ea7cc81c4806c8
taejinii <xowls177@gmail.com>
2023-03-07 23:31:42 +0900

    first commit

--- server.js
+++ server.js
@@ -5,17 +5,10 @@ const router = jsonServer.router("db/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-	// Add custom route here if needed
-	jsonServer.rewriter({
-		"/api/*": "/$1",
-	})
-);
+
 server.use(router);
 server.listen(3000, () => {
-	console.log("JSON Server is running");
+  console.log("JSON Server is running");
 });
 
-// Export the Server API
 module.exports = server;
