4584790e27f5e7b9953d40243b57388c64b57ce3
taejinii <xowls177@gmail.com>
2023-03-09 02:10:43 +0900

    server.js 수정

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-// JSON Server module
+const fs = require("fs");
+const os = require("os");
+
+fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+  if (err) console.log(err);
+  else console.log("copy success");
+});
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
